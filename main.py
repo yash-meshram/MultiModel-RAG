d022ee028b89b81307ab88d4286293672ddf3572
yash meshram <yashmeshram4102000@gmail.com>
2026-04-19 20:06:58 +0530

    Integrated Hybrid search - faiss (vector search) + typesense (keyword search)

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
+import typesense
+from sentence_transformers import CrossEncoder
 
 load_dotenv("./.env")
 
@@ -88,7 +90,34 @@ def describe_image_with_llm(img_base64):
     response = llm.invoke([message])
     return response.content
     
-    
+# defining client for typsense search
+client = typesense.Client({
+    'nodes': [{
+        'host': 'zqym3ix1p94j2r6vp-1.a2.typesense.net',  # For Typesense Cloud use xxx.a1.typesense.net
+        'port': '443',       # For Typesense Cloud use 443
+        'protocol': 'https'    # For Typesense Cloud use https
+    }],
+    'api_key': 'xxxxxxxxxxxxxxxxxxxxxx',
+    'connection_timeout_seconds': 2
+})
+
+# Create collection schema
+schema = {
+    "name": "pdf_docs",
+    "fields": [
+        {"name": "id", "type": "string"},
+        {"name": "content", "type": "string"},
+        {"name": "page", "type": "int32"},
+        {"name": "type", "type": "string", "facet": True},
+        {"name": "image_id", "type": "string", "optional": True}
+    ],
+    "default_sorting_field": "page"
+}
+client.collections.create(schema)
+
+# define LLM
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
 # Process pdf
 pdf_path = "data/Attention is all you need.pdf"
 document = fitz.open(pdf_path)
@@ -186,11 +215,54 @@ vector_store_image = FAISS.from_embeddings(
     metadatas = [doc.metadata for doc in all_images_doc]
 )
 
-# define LLM
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+# adding data in client collection
+typesense_docs = []
+for i, doc in enumerate(all_document):
+    typesense_docs.append({
+        'id': str(i),
+        'content': doc.page_content,
+        'page': doc.metadata.get('page', -1),
+        'type': doc.metadata.get('type', 'text')
+    })
+for i, doc in enumerate(all_images_doc):
+    typesense_docs.append({
+        'id': str(i + len(all_document)),
+        'content': doc.page_content,
+        'page': doc.metadata.get('page', -1),
+        'type': doc.metadata.get('type', 'text'),
+        'image_id': doc.metadata.get('image_id', '-1')
+    })
+client.collections["pdf_docs"].documents.import_(typesense_docs)
 
-# Retriever
+# keyword search from typsense
+def keyword_search(query, k = 5):
+    search_parameters = {
+        'q': query,
+        'query_by': 'content',
+        'per_page': k
+    }
+    
+    results = client.collections["pdf_docs"].documents.search(search_parameters)
+    
+    hits = []
+    for hit in results['hits']:
+        hits.append({
+            'content': hit['document']['content'],
+            'page': hit['document']['page'],
+            'type': hit['document']['type'],
+            'image_id': hit['document'].get('image_id', None),
+        })
+    
+    return hits
+
+
+# reranker - will provide score - Good balance of speed + accuracy
+reranker = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+
+# Retriever - Hybris search
 def multimodel_retriever(query, k = 5):
+    # 1. Vector Search
+    # -----------------
     # Embeded query using CLIP
     query_embedding = embed_text(query)
     
@@ -201,10 +273,74 @@ def multimodel_retriever(query, k = 5):
     )
     results_ = vector_store_image.similarity_search_by_vector(
         embedding=query_embedding,
-        k = 2
+        k = k
     )
-    r = results + results_
-    return r
+    vector_results = results + results_
+    
+    vector_hits = [{
+        'doc': doc,
+        'content': doc.page_content,
+        'source': 'vector'
+    } for doc in vector_results]
+    
+    # 2. Typesense search - search by keywords
+    # ----------------------------------------
+    keyword_hits_raw = keyword_search(query, k)
+    
+    keyword_hits = []
+    for item in keyword_hits_raw:
+        doc = Document(
+            page_content = item['content'],
+            metadata = {
+                'page': item.get('page', -1),
+                'type': item.get('type', 'text'),
+                'image_id': item.get('image_id', '-1')
+            }
+        )
+        
+        keyword_hits.append({
+            'doc': doc,
+            'content': item['content'],
+            'source': 'keyword'
+        })
+    
+    # 3. Merge both retrieved result/hit + deduplicate
+    # -------------------------------------------------
+    combine_hits = vector_hits + keyword_hits
+    
+    seen = set()
+    unique_results = []
+    
+    for item in combine_hits:
+        if item['content'] not in seen:
+            seen.add(item['content'])
+            unique_results.append(item)
+            
+    # 4. Cross-Encoder reranking
+    # ---------------------------
+    pairs = [(query, item['content']) for item in unique_results]
+    
+    scores = reranker.predict(pairs)
+    
+    for i, item in enumerate(unique_results):
+        item['score'] = float(scores[i])
+        
+    # 5. Sort
+    # --------
+    reranked_results = sorted(unique_results, key=lambda x: x['score'], reverse=True)
+    
+    # 6. Return DOcument
+    # -------------------
+    final_retrieved_docs = []
+    
+    for item in reranked_results[:k]:
+        doc = item['doc']
+        doc.metadata['score'] = item['score']
+        doc.metadata['retrieved_from'] = item['source']
+        
+        final_retrieved_docs.append(doc)
+    
+    return final_retrieved_docs
 
 def multimodel_prompt(query, retrieved_docs):
     content = []
@@ -218,6 +354,7 @@ def multimodel_prompt(query, retrieved_docs):
                 Amswer the query based on the provided text and image ONLY.
                 Use only given information, dont use any other information even if asked to do so.
                 Include image in your response if query is related to image.
+                If you dont know the answer just say - 'No infrmation available'.
                 """   
         }
     )
@@ -310,20 +447,21 @@ def multimodel_pdf_rag_pipeline(query_):
     # get response
     response = llm.invoke([prompt])
     
-    retrieved_image_docs = [doc for doc in retrieved_docs if doc.metadata.get("type") == "image"]
-    seen_images = set()
-    for doc in retrieved_image_docs:
-        image_id = doc.metadata["image_id"]
-        if image_id and image_id in all_images and image_id not in seen_images:
-            seen_images.add(image_id)
-            image_bytes = base64.b64decode(all_images[image_id])
-            image = Image.open(io.BytesIO(image_bytes))
-            image.show()  
+    if response.content != 'No information available.':
+        retrieved_image_docs = [doc for doc in retrieved_docs if doc.metadata.get("type") == "image"]
+        seen_images = set()
+        for doc in retrieved_image_docs:
+            image_id = doc.metadata["image_id"]
+            if image_id and image_id in all_images and image_id not in seen_images:
+                seen_images.add(image_id)
+                image_bytes = base64.b64decode(all_images[image_id])
+                image = Image.open(io.BytesIO(image_bytes))
+                image.show()  
     
     return response.content, retrieved_image_docs
 
 if __name__ == "__main__":
-    query = "Full transformer image"
+    query = "Explain transformer each and every steps with images"
     response, retrieved_image_docs = multimodel_pdf_rag_pipeline(query)
     print(response)
     print(retrieved_image_docs)
